50aec98567c6362d016ae3dfd8798e36100a851c
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-03-17 21:26:59 -0600

    fixing associations

--- models/index.js
+++ models/index.js
@@ -1,18 +1,18 @@
 // import models
-const Product = require("./Product");
 const Category = require("./Category");
+const Product = require("./Product");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
-
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id",
-});
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
   // onDelete: "CASCADE",
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
@@ -24,8 +24,8 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, { through: "ProductTag" });
 
 module.exports = {
-  Product,
   Category,
+  Product,
   Tag,
   ProductTag,
 };
