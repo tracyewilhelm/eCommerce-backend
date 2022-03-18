3d753a8a815d2a97fbfd33484f36c7110871bc7f
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-03-17 20:19:49 -0600

    adjusting associations

--- models/index.js
+++ models/index.js
@@ -6,21 +6,22 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: category_id,
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: category_id,
-  onDelete: "CASCADE",
+  foreignKey: "category_id",
+  // onDelete: "CASCADE",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
+  through: "ProductTag",
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: "ProductTag" });
 
 module.exports = {
   Product,
